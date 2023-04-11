4bf9ad16907ba2b028cae0f9f591782681a75e79
Karina Vargas <vargasriosk@gmail.com>
2023-04-11 17:32:14 -0500

    update method working

--- proyecto-1/controllers/productController.js
+++ proyecto-1/controllers/productController.js
@@ -32,9 +32,28 @@ const addProduct = async (req, res) => {
 
 const updateProductById = async (req, res) => {
 	try {
-		const productId = products.find((product) => product.id === req.params.id);
-		if (productId) res.status(200).json(productId);
-		res.status(404).json(`Product ${req.params.id} not found`);
+		const fileData = await fs.promises.readFile(filePath, "utf-8");
+		const products = await JSON.parse(fileData);
+
+		const { name, description, price, quantity, category } = req.body;
+
+		const productToUpdate = products.map((product) => {
+			if (product.id === req.params.id) {
+				return {
+					id: product.id,
+					name: name ? name : product.name,
+					description: description ? description : product.description,
+					price: price ? price : product.price,
+					quantity: quantity ? quantity : product.quantity,
+					category: category ? category : product.category,
+				};
+			}
+		});
+
+		return res.status(200).json({
+			success: `Product has been updated`,
+			productToUpdate,
+		});
 	} catch (error) {
 		res.status(500).json({ error: error });
 	}
@@ -44,14 +63,21 @@ const deleteProductById = async (req, res) => {
 	try {
 		const fileData = await fs.promises.readFile(filePath, "utf-8");
 		const products = await JSON.parse(fileData);
-		const productsWithoutDeleted = products.filter(
-			(product) => product.id !== req.params.id
-		);
-		await fs.promises.writeFile(
-			filePath,
-			JSON.stringify(productsWithoutDeleted)
-		);
-		res.status(200).json(`Delected ${productsWithoutDeleted.id} successfully`);
+
+		const productId = products.find((product) => product.id === req.params.id);
+		if (productId) {
+			const productsWithoutDeleted = products.filter(
+				(product) => product.id !== req.params.id
+			);
+			await fs.promises.writeFile(
+				filePath,
+				JSON.stringify(productsWithoutDeleted)
+			);
+			return res.status(200).json(`Delected ${req.params.id} successfully`);
+		}
+		return res
+			.status(404)
+			.json(`Product with id: ${req.params.id} could not be deleted not found`);
 	} catch (error) {
 		res.status(500).json({ error: error });
 	}
