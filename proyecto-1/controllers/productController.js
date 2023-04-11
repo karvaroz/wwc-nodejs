9a51b8eed9383a21465e7b899c69498bba04d635
Karina Vargas <vargasriosk@gmail.com>
2023-04-11 16:40:59 -0500

    delete method working

--- proyecto-1/controllers/productController.js
+++ proyecto-1/controllers/productController.js
@@ -3,39 +3,55 @@ const path = require("path");
 const { randomUUID } = require("crypto");
 
 const filePath = path.resolve(`${"./db"}/productsDb.txt`);
-console.log(filePath);
 
 const getAllProducts = async (req, res) => {
 	try {
-		const data = await fs.promises.readFile(filePath, "utf-8");
-		const products = JSON.parse(data);
-		res.status(200).json(products);
+		const fileData = await fs.promises.readFile(filePath, "utf-8");
+		const products = await JSON.parse(fileData);
+		if (products) res.status(200).json(products);
 	} catch (error) {
 		res.status(500).json({ error: error });
 	}
 };
 
-const addProduct = (req, res) => {
+const addProduct = async (req, res) => {
 	try {
-		const { name, description, price, quantity, category } = req.body;
-		console.log(req.body);
-		res.status(200).json({ success: true });
+		const newProduct = {
+			id: randomUUID(),
+			...req.body,
+		};
+		const fileData = await fs.promises.readFile(filePath, "utf-8");
+		const products = await JSON.parse(fileData);
+		products.push(newProduct);
+		await fs.promises.writeFile(filePath, JSON.stringify(products));
+		res.status(201).json(newProduct);
 	} catch (error) {
 		res.status(500).json({ error: error });
 	}
 };
 
-const updateProductById = (req, res) => {
+const updateProductById = async (req, res) => {
 	try {
-		res.status(200).json({ success: true });
+		const productId = products.find((product) => product.id === req.params.id);
+		if (productId) res.status(200).json(productId);
+		res.status(404).json(`Product ${req.params.id} not found`);
 	} catch (error) {
 		res.status(500).json({ error: error });
 	}
 };
 
-const deleteProductById = (req, res) => {
+const deleteProductById = async (req, res) => {
 	try {
-		res.status(200).json({ success: true });
+		const fileData = await fs.promises.readFile(filePath, "utf-8");
+		const products = await JSON.parse(fileData);
+		const productsWithoutDeleted = products.filter(
+			(product) => product.id !== req.params.id
+		);
+		await fs.promises.writeFile(
+			filePath,
+			JSON.stringify(productsWithoutDeleted)
+		);
+		res.status(200).json(`Delected ${productsWithoutDeleted.id} successfully`);
 	} catch (error) {
 		res.status(500).json({ error: error });
 	}
